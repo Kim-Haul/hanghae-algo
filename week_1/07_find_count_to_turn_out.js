6f49c1d689b2753330626b17a28742b62512af26
전인호 <bokugawa@naver.com>
2022-10-15 22:57:06 +0900

    1주차 알고리즘 풀이(2)

--- week_1/07_find_count_to_turn_out.js
+++ week_1/07_find_count_to_turn_out.js
@@ -1,4 +1,4 @@
-input = "011110";
+input = "11110111101";
 
 const find_count_to_turn_out_to_all_zero_or_all_one = (string) => {
   let cnt_to_all_zero = 0;
@@ -6,8 +6,21 @@ const find_count_to_turn_out_to_all_zero_or_all_one = (string) => {
 
   for (i = 0; i < string.length - 1; i++) {
     if (string[i] != string[i + 1]) {
+      if (string[i + 1] == "0") {
+        cnt_to_all_one += 1;
+      } else if (string[i + 1] == "1") {
+        cnt_to_all_zero += 1;
+      }
     }
   }
+
+  if (string[0] == 1) {
+    cnt_to_all_zero += 1;
+  } else {
+    cnt_to_all_one += 1;
+  }
+
+  return Math.min(cnt_to_all_zero, cnt_to_all_one);
 };
 
 result = find_count_to_turn_out_to_all_zero_or_all_one(input);
