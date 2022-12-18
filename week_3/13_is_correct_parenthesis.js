9b9c2ccc52abbb31ca395a800659e6a1902a5d5a
Kim-Haul <bokugawa@naver.com>
2022-12-19 01:48:27 +0900

    3주차 알고리즘 풀이(7)

--- week_3/13_is_correct_parenthesis.js
+++ week_3/13_is_correct_parenthesis.js
@@ -1,5 +1,24 @@
-s = "(())()";
+s = "())";
 
-const is_correct_parenthesis = (string) => {};
+const is_correct_parenthesis = (string) => {
+  let stack = 0;
+  for (i = 0; i < s.length; i++) {
+    if (s[i] == "(") {
+      stack += 1;
+    } else {
+      stack -= 1;
+    }
 
-print(is_correct_parenthesis(s));
+    if (stack < 0) {
+      return false;
+    }
+  }
+
+  if (stack == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(is_correct_parenthesis(s));
--- week_3/14_get_melon_best_album.js
+++ week_3/14_get_melon_best_album.js
@@ -1,6 +1,23 @@
 genres = ["classic", "pop", "classic", "classic", "pop"];
 plays = [500, 600, 150, 800, 2500];
 
-const get_melon_best_album = (genre_array, play_array) => {};
+const get_melon_best_album = (genre_array, play_array) => {
+  let genre_total_paly_dict = {};
+  let genre_index_paly_array_dict = {};
+
+  for (i = 0; i < genre_array.length; i++) {
+    // 자바스크립트 객체안에 key & value 쌍을 추가할 수 있는 새로운 방법. 기존에 몰랐음.
+    // dict.genre_array[i] = play_array[i]가 안돼서 js에서는 python의 딕셔너리를 완전히 표현 못하나 싶었는데 가능.. 충격.
+    if (genre_total_paly_dict[genre_array[i]] == undefined) {
+      // 초기값에 대한 설정 고려 필요. undefined + number = NaN 이기 때문.
+      genre_total_paly_dict[genre_array[i]] = play_array[i];
+      genre_index_paly_array_dict[genre_array[i]] = [[i, play_array[i]]];
+    } else {
+      genre_total_paly_dict[genre_array[i]] += play_array[i];
+      genre_index_paly_array_dict[genre_array[i]].push([i, play_array[i]]);
+    }
+  }
+  return genre_total_paly_dict;
+};
 
 console.log(get_melon_best_album(genres, plays));
